8135e04ba42e6366aa0306ef2417eac050bec25c
cartacioS <scart039@fiu.edu>
2018-09-15 09:31:54 -0400

    Server API Call Skeleton

--- VoiceDropBackEnd/index.js
+++ VoiceDropBackEnd/index.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const uuid = require('uuid');
-const AWS = require('aws-sdk);
+const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
 	console.log(event);
+	if(event.context['https-method'] === 'POST') {
+		postData(event, context, callback);
+	} else if (event.context['https-method'] === 'GET') {
+		getData(event, context, callback);
+	}
 	const response = {
-		statusCode: 200,
-		bode: JSON.stringify({
-			message 'Hello, the current time is ${new Date().toTimeString()}.',
-		}),
+		statusCode: 200
 	};
-	callback(null, response);
+
+	return callback(null, response);
+}
+
+function putData(event, context, callback) {
+	console.log("Putting!");
+}
+
+function getData(event, context, callback) {
+	console.log("Getting!");
 }
