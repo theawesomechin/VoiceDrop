0098d0578af1f56b73052f9ed77f032ff8e24768
Macbook <nalinsuri@gmail.com>
2018-09-16 00:32:05 -0400

    Got post working

--- VoiceDropBackEnd/postLambda/index.js
+++ VoiceDropBackEnd/postLambda/index.js
@@ -2,12 +2,12 @@
 
 const uuid = require('uuid/v1');
 const AWS = require('aws-sdk');
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+// const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
 const AWS_REGION = "us-east-1";
-const S3_BUCKET_NAME = "voicedrop-lambda-files";
-// AWS.config.update({region: 'REGION'});
+const S3_BUCKET_NAME = "voicedrop-audio-recordings";
+AWS.config.update({region: AWS_REGION});
 const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
 const RECORDING_TABLE_NAME = "recordings";
 
@@ -18,23 +18,28 @@ exports.handler = (event, context, callback) => {
 	};
 
 	return callback(null, response);
-}
+};
 
 function postData(event, context, callback) {
-	let encodedData = "DUMMY DATA"
+	let encodedData;
+	if (event.voiceClip === undefined) {
+		exitAsInternalServerError("ERROR: voiceClip undefined", callback);
+	} else {
+		encodedData = event.voiceClip;
+	}
+	
 	let decodedData = Buffer.from(encodedData, 'base64');
-	console.log('decodedData', decodedData);
-	let s3Upload = "";
+	let s3Upload = uuid();
 	try {
-		// s3Upload = uploadDataToS3(decodedData);
-		addToDynamoDB(-80.376229, 25.760547, "Nalin", s3Upload);
+		s3Upload = uploadDataToS3(decodedData);
+		addToDynamoDB(event.longitude, event.latitude, event.userName, s3Upload);
 	} catch (err) {
-		callback(err, {"statusCode": 500});
+		exitAsInternalServerError(err, callback);
 	}
 }
 
 function uploadDataToS3(dataToUpload) {
- 	var filePath = uuid() + '.jpg';
+ 	var filePath = uuid() + '.mp3';
 	var params = {
 		"Body": dataToUpload,
 		"Bucket": S3_BUCKET_NAME,
@@ -54,11 +59,11 @@ function addToDynamoDB(longitude, latitude, name, s3Path) {
 	const recordingRelation = {
 	  TableName: RECORDING_TABLE_NAME,
 	  Item: {
-	    'timestamp' : {N: Date.now()},
-	    'longitude' : {N: longitude},
-	    'latitude' : {N: latitude},
-	    'name' : {S: name},
-	    'voteCount' : {N: 0},
+	  	'timestamp' : {N: Date.now().toString()},
+	    'longitude' : {N: longitude.toString()},
+	    'latitude' : {N: latitude.toString()},
+	    'name' : {S: name || 'unknown'},
+	    'voteCount' : {N: '0'},
 	    's3Link' : {S: s3Path},
 	  }
 	};
@@ -66,12 +71,12 @@ function addToDynamoDB(longitude, latitude, name, s3Path) {
 	// Call DynamoDB to add the item to the table
 	ddb.putItem(recordingRelation, function(err, data) {
 	  if (err) {
-	    console.log("Error", err);
-	  } else {
-	    console.log("Success", data);
+	    throw err;
 	  }
 	});
 }
 
-
+function exitAsInternalServerError(err, callback) {
+	callback(err, {"statusCode": 500});
+}
 
